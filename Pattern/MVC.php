b4e908ec7558b60b0f5458b9b984fc2c8726495e
srcspider <source.spider@gmail.com>
2012-03-22 15:54:16 +0200

    MVC interface changed to Pattern_MVC interface

--- Pattern/MVC.php
+++ Pattern/MVC.php
@@ -3,13 +3,15 @@
 /** 
  * Common Language Interface
  * 
+ * Model-View-Controller pattern.
+ * 
  * @package    Kohana4
  * @category   Types
  * @author     Kohana Team
  * @copyright  (c) 2008-2012 Kohana Team
  * @license    http://kohanaframework.org/license
  */
-interface MVC
+interface Pattern_MVC
 {
 	// the name to be used in layers implementing the interface; the reason you
 	// wouldn't just use say "\kohana4\types\MVC" is beause any implementation
