9b2bbe32d2505f2d214acdc49fe399ed1e15a15b
srcspider <source.spider@gmail.com>
2012-04-06 17:34:21 +0300

    requirements enum

---
+++ Enum/Requirement.php
@@ -0,0 +1,23 @@
+<?php namespace ibidem\types;
+
+/** 
+ * Common Language Interface
+ * 
+ * @package    ibidem
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2008-2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface Enum_Requirement
+{
+	// ideal settings available
+	const available = '\ibidem\types\Enum_Requirement::available';
+	// ideal settings not available but backup available; ie. will work
+	const failed = '\ibidem\types\Enum_Requirement::failed';
+	// minimum requirements not met; behaviour is undefined
+	const error = '\ibidem\types\Enum_Requirement::error';
+	// test fails with exception; equivalent to error in severity
+	const untestable = '\ibidem\types\Enum_Requirement::untestable';
+	
+} # interface
