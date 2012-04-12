e1497b0c756d24d960882799561008f175ce376d
srcspider <source.spider@gmail.com>
2012-04-12 18:29:57 +0300

    date enum

---
+++ Date.php
@@ -0,0 +1,19 @@
+<?php namespace ibidem\types;
+
+/** 
+ * @package    ibidem
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface Date
+{
+	const year = 31536000; // vague year (365 days)
+	const month = 2592000; // vague month (30 days)
+	const week = 604800;
+	const day = 86400;
+	const hour = 3600;
+	
+} # interface
+
