b618b4fc8cf67270f12cf55a94880fa42effd6f0
srcspider <source.spider@gmail.com>
2012-04-18 14:22:13 +0300

    pager interface

---
+++ Pager.php
@@ -0,0 +1,75 @@
+<?php namespace ibidem\types;
+
+/** 
+ * Common Language Interface
+ * 
+ * @package    ibidem
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface Pager
+{
+	const ascending  = 'asc';
+	const descending = 'dsc';
+	
+	/**
+	 * @param integer page
+	 * @return \ibidem\types\Pager $this 
+	 */
+	function currentpage($page);
+	
+	/**
+	 * @param integer page
+	 * @return \ibidem\types\Pager $this 
+	 */
+	function bookmark($page);
+	
+	/**
+	 * Generic anchor; for example, in the case of html this will result in
+	 * a '#'.$anchor in the url; other systems might behave differently.
+	 * 
+	 * @param string anchor
+	 * @return \ibidem\types\Pager $this
+	 */
+	function bookmark_anchor($anchor);
+	
+	/**
+	 * @param string plural version
+	 * @param string singular version; assumed plural if not specified
+	 * @return \ibidem\types\Pager $this
+	 */
+	function entry_name($plural, $singular = null);
+	
+	/**
+	 * @param string order
+	 * @return \ibidem\types\Pager $this
+	 */
+	function order($order);
+	
+	/**
+	 * @param integer total items
+	 * @return \ibidem\types\Pager $this
+	 */
+	function totalitems($totalitems);
+	
+	/**
+	 * @param string base_url
+	 * @return \ibidem\types\Pager $this
+	 */
+	function url_base($url_base);
+
+	/**
+	 * @param integer pagediff
+	 * @return \ibidem\types\Pager $this 
+	 */
+	function pagediff($pagediff);
+	
+	/**
+	 * @param integer pagelimit
+	 * @return \ibidem\types\Pager $this
+	 */
+	function pagelimit($pagelimit);
+	
+} # interface
