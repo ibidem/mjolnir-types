8365d38c06466599f93d7d47dc35916d6547bdc7
srcspider <source.spider@gmail.com>
2012-11-12 18:16:45 +0200

    executable

---
+++ Executable.php
@@ -0,0 +1,19 @@
+<?php namespace mjolnir\types;
+
+/** 
+ * Common Language Interface
+ * 
+ * @package    mjolnir
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface Executable
+{
+	/**
+	 * Execute the object.
+	 */
+	function execute();
+	
+} # interface
