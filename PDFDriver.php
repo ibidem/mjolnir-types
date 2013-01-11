3339983cf7f8a138083e31b3cd93024c442fc1e3
srcspider <source.spider@gmail.com>
2013-01-11 20:04:10 +0200

    PDFDriver interface

---
+++ PDFDriver.php
@@ -0,0 +1,24 @@
+<?php namespace mjolnir\types;
+
+/**
+ * Common Language Interface
+ *
+ * @package    mjolnir
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2012 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface PDFDriver
+{
+	/**
+	 * @return string pdf
+	 */
+	function from_html($html);
+	
+	/**
+	 * Stream pdf to client.
+	 */
+	function stream($html, $filename);
+
+} # interfacea
