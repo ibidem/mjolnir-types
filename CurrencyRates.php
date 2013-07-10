ed93117147d270a9708ffc2d5f2d11b46025eb1a
srcspider <source.spider@gmail.com>
2013-07-10 11:38:30 +0300

    CurrencyRates driver type

---
+++ CurrencyRates.php
@@ -0,0 +1,21 @@
+<?php namespace mjolnir\types;
+
+/**
+ * This is a driver interface. See: \app\Currency
+ * 
+ * @package    mjolnir
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2013 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+interface CurrencyRates
+{
+	/**
+	 * Insert rate information into given types array and pass it back.
+	 * 
+	 * @return array
+	 */
+	function process($types);
+
+} # interface
---
+++ Trait/CurrencyRates.php
@@ -0,0 +1,14 @@
+<?php namespace mjolnir\types;
+
+/**
+ * @package    mjolnir
+ * @category   Types
+ * @author     Ibidem Team
+ * @copyright  (c) 2013 Ibidem Team
+ * @license    https://github.com/ibidem/ibidem/blob/master/LICENSE.md
+ */
+trait Trait_CurrencyRates
+{
+	// trait serves as placeholder for extended functionality injection
+
+} # trait
